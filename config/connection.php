ee0b8ee37a9dbd5a1c1f63d1c19c5c8b7cc4a841
Mohasalama <msalama2007@hotmail.com>
2020-08-30 19:08:07 -0400

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -4,12 +4,22 @@
 
     // LOCAL CONNECTION
 
-    $db_user = "root";
-    $db_pass = "";
-    $db_name = "warsha_shop";
-    $db_host = "localhost";
+    // $db_user = "root";
+    // $db_pass = "";
+    // $db_name = "warsha_shop";
+    // $db_host = "localhost";
     
    
+    // $con = new mysqli($db_host,$db_user,$db_pass,$db_name);
+
+
+    // HEROKU
+
+    $db_user = "b1ccf8cb2ba2ed";
+    $db_pass = "64d7bb8b";
+    $db_name = "heroku_f35d69e42e8fd60";
+    $db_host = "us-cdbr-east-02.cleardb.com";
+
     $con = new mysqli($db_host,$db_user,$db_pass,$db_name);
 
    
