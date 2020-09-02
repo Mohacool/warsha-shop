3ea663f4ca1cf1d1db1539c04e1ef300e7ae2e12
Mohasalama <msalama2007@hotmail.com>
2020-09-01 22:25:00 -0400

    rename grid to products

--- products.php
+++ products.php
@@ -1,115 +1,108 @@
 <?php 
-    session_start();
-
-    $db_user = "root";
-    $db_pass = "";
-    $db_name = "warsha_shop";
-    $db_host = "localhost";
-
-    $show_squares=true;
-
-    $con = new mysqli($db_host,$db_user,$db_pass,$db_name);
-
-    if (isset($_GET['product_id']) && $_GET['product_id']!=''){
-        $show_squares=false;
-    }
-
-    header('Cache-Control: no cache'); //disable validation of form by the browser
-    
-
-
+require('config/connection.php');
+$media_url = 'media/products/';
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Our Products</title>
+    
+    <!-- Import Bootstrap,Jquery... -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <script
+        src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="grid_style.css">
 </head>
 <body>
-    <style>
-        .square{
-            height:300px;
-            width:300px;
-            border:2px solid black;
-            display:inline-block;
-            align:center;
-        }
+    
 
-        .text-example { 
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            flex-direction: column;
-        }
-        .badge{
-            border-radius:100%;
-            padding:10px;
-            background-color:cyan;
-            margin-top:15px;
 
-        }
-        .button{
-            text-decoration:none;
-            color:black;
-        }
+    <!-- HEADER -->
+    <?php include('main_header.php') ?>
+    
+    <div class="container-fluid show">
+        
+        <br class="pt-5"><br class="pt-5">
+        <div class="row px-5 pt-5">
+
+             <?php 
+                    
+
+                $query = mysqli_query($con,"select * from product");
+
+                while($row=mysqli_fetch_assoc($query)){
+                    if ($row['image'] != ''){
+                        $img_url = $media_url. '' .$row['image'];
+                        $item_name = $row['name'];
+                        $id = $row['id'];
+
+                        if(strpos($item_name,'<br>')!== false){
+                            // echo "not found new line";
+                        }
+                        else{
+                            // echo "added newline";
+                            $item_name .= "<br><br>";
+                        }
+
+                        echo "
+                        <div class='col-lg-4 col-sm-6 d-flex pb-4 px-3' style='align-items:center'>
+
+                            <a href='product_page?id=$id'>
+                            <div class='col nopadding nomargin'>
+        
+                                <div class='over'>
+                                    <div class='row nopadding nomargin h-100'>
+                                        <div class='w-100 d-flex align-items-center'>
+                                            <p class='myp nomargin nopadding'>$item_name</p>
+                                        </div>
+                                    </div>
+                                </div>
+        
+                                <img class='d-block nomargin nopadding try' src='$img_url' width='100%'>
+                                <div class='row'>
+                                    <p class='mymobilep mx-auto pt-2'>$item_name</p>
+                                </div>
+        
+                            </div>
+                            </a>
+        
+                        </div>
+                            
+                        
+                            ";
+                    }
+                    
+                }
+ 
+
+            ?> 
+                    
+
+                
+              
+                
+               
+        </div>
 
         
-    </style>
-    
-    <nav style="background-color:grey;height:50px">
 
-        <h1>
-            Products Below
-        </h1>
-        <a class="cart" href="#" style="float:right;margin-top:-40px;">
-            <img src="https://img.icons8.com/cotton/64/000000/shopping-cart--v2.png" width="28px" style="cursor:pointer;">
-            <span class="badge">1</span>
-        </a>
-    </nav>
-    <?php 
-
-        $query = mysqli_query($con,"select * from test_products order by name asc");
-        if ($show_squares==true){
-
-            while($row=mysqli_fetch_assoc($query)){
-                $name = $row['name'];
-                $image = $row['image'];
-                $id = $row['id'];
-                
-                echo "
-                <div class='square'>
-                    <h3>$name</h3>
-                    <img src='$image' width='100px'>
-                    <form method='post' action='products.php'>
-                        <button> <a class='button' href='product_zoom.php?product_id=$id'>View </a></button>
-                    </form>
-                    
-                </div>
-                
-                ";
-            }
-
-        }
-       
-
-    ?>
-    <!-- <div class="square">
-        <h3 class="text-example">Shoe</h3>
-        <img src="https://images-na.ssl-images-amazon.com/images/I/61utX8kBDlL._UL1100_.jpg" width="100px">
-        <button>Add to cart</button>
     </div>
-    <div class="square">
-        <h3 class="text-example">Potato Bag</h3>
-        <img src="https://www.totavo.com/1051-large/white-potatoes-bag-of-10lb.jpg" width="100px">
-        <button>Add to cart</button>
-
-
-    </div> -->
 
     
+
     
-    
+
+
+    <!-- FOOTER -->
+    <?php include('main_footer.php') ?>
     
 </body>
 </html>
