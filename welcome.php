0253542555d3e3a17f335432877225f50a0fa504
Mohasalama <msalama2007@hotmail.com>
2020-09-04 12:25:09 -0400

    google verification

--- welcome.php
+++ welcome.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta name="google-site-verification" content="1EhQCuXpaWE6fIeYlFO9HDkVy6vLysFiT5AVHtjRUTc" />
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Warsha Shop</title>
